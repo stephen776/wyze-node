81ad87442787ceec2a5203a17468e1be552ca78b
Stephen Jacobs <sjacobs@truefit.io>
2021-01-31 15:24:04 -0500

    remove localStorge

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-'use strict'
-const axios = require('axios')
-const md5 = require('md5')
-const moment = require('moment')
-const LocalStorage = require('node-localstorage').LocalStorage
-const localStorage = new LocalStorage('./scratch')
-
+'use strict';
+const axios = require('axios');
+const md5 = require('md5');
+const moment = require('moment');
 class Wyze {
   /**
    * @param {object} options
    * @constructor
    */
   constructor(options) {
-    this.username = options.username
-    this.password = options.password
-    this.phoneId = options.phoneId || 'bc151f39-787b-4871-be27-5a20fd0a1937'
-    this.baseUrl = options.baseUrl || 'https://api.wyzecam.com:8443'
-    this.appVer = options.appVer || 'com.hualai.WyzeCam___2.3.69'
-    this.sc = '9f275790cab94a72bd206c8876429f3c'
-    this.sv = '9d74946e652647e9b6c9d59326aef104'
-    this.accessToken = options.accessToken || ''
-    this.refreshToken = options.refreshToken || ''
+    this.username = options.username;
+    this.password = options.password;
+    this.phoneId = options.phoneId || 'bc151f39-787b-4871-be27-5a20fd0a1937';
+    this.baseUrl = options.baseUrl || 'https://api.wyzecam.com:8443';
+    this.appVer = options.appVer || 'com.hualai.WyzeCam___2.3.69';
+    this.sc = '9f275790cab94a72bd206c8876429f3c';
+    this.sv = '9d74946e652647e9b6c9d59326aef104';
+    this.accessToken = options.accessToken || '';
+    this.refreshToken = options.refreshToken || '';
   }
 
   /**
-  * get request data
-  */
+   * get request data
+   */
   async getRequestBodyData(data = {}) {
     return {
       access_token: this.accessToken,
@@ -34,25 +31,17 @@ class Wyze {
       sv: this.sv,
       ts: moment().unix(),
       ...data,
-    }
+    };
   }
 
   /**
-  * get tokens
-  */
-  async getTokens() {
-    this.accessToken = localStorage.getItem('access_token')
-    this.refreshToken = localStorage.getItem('refresh_token')
-  }
-
-  /**
-  * set tokens
-  */
+   * set tokens
+   */
   async setTokens(accessToken, refreshToken) {
-    localStorage.setItem('access_token', accessToken)
-    localStorage.setItem('refresh_token', refreshToken)
-    this.accessToken = accessToken
-    this.refreshToken = refreshToken
+    localStorage.setItem('access_token', accessToken);
+    localStorage.setItem('refresh_token', refreshToken);
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
   }
 
   /**
@@ -60,23 +49,27 @@ class Wyze {
    * @returns {data}
    */
   async login() {
-    console.log('Login In...')
-    let result
+    console.log('Login In...');
+    let result;
     try {
-
       const data = {
         user_name: this.username,
         password: md5(md5(this.password)),
-      }
+      };
 
-      result = await axios.post(`${this.baseUrl}/app/user/login`, await this.getRequestBodyData(data))
-      this.setTokens(result.data.data['access_token'], result.data.data['refresh_token'])
+      result = await axios.post(
+        `${this.baseUrl}/app/user/login`,
+        await this.getRequestBodyData(data),
+      );
+      this.setTokens(
+        result.data.data['access_token'],
+        result.data.data['refresh_token'],
+      );
+    } catch (e) {
+      console.log('login error...', e);
+      throw e;
     }
-    catch (e) {
-      console.log('login error...', e)
-      throw e
-    }
-    return result.data
+    return result.data;
   }
 
   /**
@@ -84,20 +77,25 @@ class Wyze {
    * @returns {data}
    */
   async getRefreshToken() {
-    console.log('Refreshing token...')
-    let result
+    console.log('Refreshing token...');
+    let result;
     try {
       const data = {
         refresh_token: this.refreshToken,
-      }
-      result = await axios.post(`${this.baseUrl}/app/user/refresh_token`, await this.getRequestBodyData(data))
-      this.setTokens(result.data.data['access_token'], result.data.data['refresh_token'])
+      };
+      result = await axios.post(
+        `${this.baseUrl}/app/user/refresh_token`,
+        await this.getRequestBodyData(data),
+      );
+      this.setTokens(
+        result.data.data['access_token'],
+        result.data.data['refresh_token'],
+      );
+    } catch (e) {
+      console.log('refreshToken error...', e);
+      throw e;
     }
-    catch (e) {
-      console.log('refreshToken error...', e)
-      throw e
-    }
-    return result.data
+    return result.data;
   }
 
   /**
@@ -105,23 +103,24 @@ class Wyze {
    * @returns {data}
    */
   async getObjectList() {
-    let result
+    let result;
     try {
-      await this.getTokens();
       if (!this.accessToken) {
-        await this.login()
+        await this.login();
       }
-      result = await axios.post(`${this.baseUrl}/app/v2/home_page/get_object_list`, await this.getRequestBodyData())
+      result = await axios.post(
+        `${this.baseUrl}/app/v2/home_page/get_object_list`,
+        await this.getRequestBodyData(),
+      );
       if (result.data.msg === 'AccessTokenError') {
-        await this.getRefreshToken()
-        return this.getObjectList()
+        await this.getRefreshToken();
+        return this.getObjectList();
       }
+    } catch (e) {
+      console.log('Error...', e);
+      throw e;
     }
-    catch (e) {
-      console.log('Error...', e)
-      throw e
-    }
-    return result.data
+    return result.data;
   }
 
   /**
@@ -129,11 +128,10 @@ class Wyze {
    * @returns {data}
    */
   async runAction(instanceId, providerKey, actionKey) {
-    let result
+    let result;
     try {
-      await this.getTokens();
       if (!this.accessToken) {
-        await this.login()
+        await this.login();
       }
 
       const data = {
@@ -142,191 +140,212 @@ class Wyze {
         action_key: actionKey,
         action_params: {},
         custom_string: '',
-      }
+      };
 
-      result = await axios.post(`${this.baseUrl}/app/v2/auto/run_action`, await this.getRequestBodyData(data))
+      result = await axios.post(
+        `${this.baseUrl}/app/v2/auto/run_action`,
+        await this.getRequestBodyData(data),
+      );
 
       if (result.data.msg === 'AccessTokenError') {
-        await this.getRefreshToken()
-        return this.runAction(instanceId, actionKey)
+        await this.getRefreshToken();
+        return this.runAction(instanceId, actionKey);
       }
+    } catch (e) {
+      console.log('Error...', e);
+      throw e;
     }
-    catch (e) {
-      console.log('Error...', e)
-      throw e
-    }
-    return result.data
+    return result.data;
   }
 
   /**
-  * get device info 
-  * @returns {data.data}
-  */
+   * get device info
+   * @returns {data.data}
+   */
   async getDeviceInfo(deviceMac, deviceModel) {
-    let result
+    let result;
     try {
-      await this.getTokens();
       if (!this.accessToken) {
-        await this.login()
+        await this.login();
       }
       const data = {
         device_mac: deviceMac,
         device_model: deviceModel,
-      }
-      result = await axios.post(`${this.baseUrl}/app/v2/device/get_device_info`, await this.getRequestBodyData(data))
+      };
+      result = await axios.post(
+        `${this.baseUrl}/app/v2/device/get_device_info`,
+        await this.getRequestBodyData(data),
+      );
     } catch (e) {
-      console.log('Error...', e)
-      throw e
+      console.log('Error...', e);
+      throw e;
     }
-    return result.data.data
+    return result.data.data;
   }
 
   /**
-  * get property
-  * @returns {data.property_list}
-  */
+   * get property
+   * @returns {data.property_list}
+   */
   async getPropertyList(deviceMac, deviceModel) {
-    let result
+    let result;
     try {
-      await this.getTokens();
       if (!this.accessToken) {
-        await this.login()
+        await this.login();
       }
       const data = {
         device_mac: deviceMac,
         device_model: deviceModel,
-      }
-      result = await axios.post(`${this.baseUrl}/app/v2/device/get_property_list`, await this.getRequestBodyData(data))
+      };
+      result = await axios.post(
+        `${this.baseUrl}/app/v2/device/get_property_list`,
+        await this.getRequestBodyData(data),
+      );
     } catch (e) {
-      console.log('Error...', e)
-      throw e
+      console.log('Error...', e);
+      throw e;
     }
-    return result.data.data.property_list
+    return result.data.data.property_list;
   }
 
   /**
-  * set property
-  * @returns {data}
-  */
+   * set property
+   * @returns {data}
+   */
   async setProperty(deviceMac, deviceModel, propertyId, propertyValue) {
-    let result
+    let result;
     try {
-      await this.getTokens();
       if (!this.accessToken) {
-        await this.login()
+        await this.login();
       }
       const data = {
         device_mac: deviceMac,
         device_model: deviceModel,
         pid: propertyId,
         pvalue: propertyValue,
-      }
-      const result = await axios.post(`${this.baseUrl}/app/v2/device/set_property`, await this.getRequestBodyData(data))
-
+      };
+      const result = await axios.post(
+        `${this.baseUrl}/app/v2/device/set_property`,
+        await this.getRequestBodyData(data),
+      );
     } catch (e) {
-      console.log('Error...', e)
-      throw e
+      console.log('Error...', e);
+      throw e;
     }
-    return result.data
+    return result.data;
   }
 
   /**
-  * Helper functions
-  */
+   * Helper functions
+   */
 
   /**
-  * getDeviceList
-  */
+   * getDeviceList
+   */
   async getDeviceList() {
-    const result = await this.getObjectList()
-    return result.data.device_list
+    const result = await this.getObjectList();
+    return result.data.device_list;
   }
 
   /**
-  * getDeviceByName
-  */
+   * getDeviceByName
+   */
   async getDeviceByName(nickname) {
-    const result = await this.getDeviceList()
-    const device = result.find(device => device.nickname.toLowerCase() === nickname.toLowerCase())
-    return device
+    const result = await this.getDeviceList();
+    const device = result.find(
+      (device) => device.nickname.toLowerCase() === nickname.toLowerCase(),
+    );
+    return device;
   }
 
   /**
-  * getDeviceByMac
-  */
+   * getDeviceByMac
+   */
   async getDeviceByMac(mac) {
-    const result = await this.getDeviceList()
-    const device = result.find(device => device.mac === mac)
-    return device
+    const result = await this.getDeviceList();
+    const device = result.find((device) => device.mac === mac);
+    return device;
   }
 
   /**
-  * getDevicesByType
-  */
+   * getDevicesByType
+   */
   async getDevicesByType(type) {
-    const result = await this.getDeviceList()
-    const devices = result.filter(device => device.product_type.toLowerCase() === type.toLowerCase())
-    return devices
+    const result = await this.getDeviceList();
+    const devices = result.filter(
+      (device) => device.product_type.toLowerCase() === type.toLowerCase(),
+    );
+    return devices;
   }
 
   /**
-  * getDevicesByModel
-  */
+   * getDevicesByModel
+   */
   async getDevicesByModel(model) {
-    const result = await this.getDeviceList()
-    const devices = result.filter(device => device.product_model.toLowerCase() === model.toLowerCase())
-    return devices
+    const result = await this.getDeviceList();
+    const devices = result.filter(
+      (device) => device.product_model.toLowerCase() === model.toLowerCase(),
+    );
+    return devices;
   }
 
   /**
-  * getDeviceGroupsList
-  */
+   * getDeviceGroupsList
+   */
   async getDeviceGroupsList() {
-    const result = await this.getObjectList()
-    return result.data.device_group_list
+    const result = await this.getObjectList();
+    return result.data.device_group_list;
   }
 
   /**
-  * getDeviceSortList
-  */
+   * getDeviceSortList
+   */
   async getDeviceSortList() {
-    const result = await this.getObjectList()
-    return result.data.device_sort_list
+    const result = await this.getObjectList();
+    return result.data.device_sort_list;
   }
 
-
   /**
-  * turnOn
-  */
+   * turnOn
+   */
   async turnOn(device) {
-    return await this.runAction(device.mac, device.product_model, 'power_on')
+    return await this.runAction(device.mac, device.product_model, 'power_on');
   }
 
   /**
-  * turnOff
-  */
+   * turnOff
+   */
   async turnOff(device) {
-    return await this.runAction(device.mac, device.product_model, 'power_off')
+    return await this.runAction(device.mac, device.product_model, 'power_off');
   }
 
   /**
-  * getDeviceStatus
-  */
+   * getDeviceStatus
+   */
   async getDeviceStatus(device) {
-    return device.device_params
+    return device.device_params;
   }
 
   /**
-  * getDeviceState
-  */
+   * getDeviceState
+   */
   async getDeviceState(device) {
-    let state = device.device_params.power_switch !== undefined ? (device.device_params.power_switch === 1 ? 'on' : 'off') : ''
+    let state =
+      device.device_params.power_switch !== undefined
+        ? device.device_params.power_switch === 1
+          ? 'on'
+          : 'off'
+        : '';
     if (!state) {
-      state = device.device_params.open_close_state !== undefined ? (device.device_params.open_close_state === 1 ? 'open' : 'closed') : ''
+      state =
+        device.device_params.open_close_state !== undefined
+          ? device.device_params.open_close_state === 1
+            ? 'open'
+            : 'closed'
+          : '';
     }
-    return state
+    return state;
   }
-
 }
 
-module.exports = Wyze
+module.exports = Wyze;
